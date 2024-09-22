545e975669853e262bb20a4291cb313db2ccc4bf
Shravan Shilwant <shravanrocks07@gmail.com>
2024-09-22 18:21:53 +0530

    added route and controller for user(setup and check)

--- src/app.js
+++ src/app.js
@@ -13,4 +13,10 @@ app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true, limit:'16kb'}))
 app.use(express.static("public"))
 
+// import Routes
+import userRouter from './routes/user.routes.js'
+
+// routes declaration
+app.use('/api/v1/user', userRouter);
+
 export {app};
